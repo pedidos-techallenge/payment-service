7a7a6fad383fde871013a2f1d4bfd67e7d8b844c
jubel <jubelcassioverridasilva@gmail.com>
2024-11-23 19:01:27 -0300

    Changing /health endpoint to accept only GET requests

--- payment/src/main/java/br/com/fiap/techchallenge/payment/infrastructure/api/HealthStatus.java
+++ payment/src/main/java/br/com/fiap/techchallenge/payment/infrastructure/api/HealthStatus.java
@@ -1,6 +1,7 @@
 package br.com.fiap.techchallenge.payment.infrastructure.api;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthStatus {
 
         @SuppressWarnings("SameReturnValue")
-        @RequestMapping("/health")
+        @GetMapping("/health")
         public String health() {
             return "OK";
         }
